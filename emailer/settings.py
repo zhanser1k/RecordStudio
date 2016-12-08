9fc07d9b0a3a2fee0d8daebe3287df0180c87a27
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2016-12-08 16:50:34 +0700

    now emails are working

--- emailer/settings.py
+++ emailer/settings.py
@@ -1,8 +1,8 @@
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sbekzatm@gmail.com'
-DEFAULT_FROM_EMAIL = 'sbekzatm@gmail.com'
-SERVER_EMAIL = 'sbekzatm@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'recordstudio.noreply@gmail.com'
+DEFAULT_FROM_EMAIL = 'recordstudio.noreply@gmail.com'
+SERVER_EMAIL = 'recordstudio.noreply@gmail.com'
+EMAIL_HOST_PASSWORD = 'RecordstudioProject'
